3ab894db9083205a5446609b7a7cbd06aae34bd2
Utkarsh-megsys <utkarsh.s@megsys.digital>
2026-02-11 21:04:55 +0530

    Add:sidebar designed

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,19 +1,15 @@
+import Sidebar from "./Sidebar";
+
 function AppLayout({ children }) {
   return (
     <div className="min-h-screen flex bg-slate-100">
-      {/* Sidebar placeholder */}
-      <aside className="w-64 bg-white border-r border-slate-200">
-        <div className="p-4 font-semibold text-lg">MailBox</div>
-      </aside>
+      <Sidebar />
 
-      {/* Main content area */}
       <main className="flex-1 flex flex-col">
-        {/* Header placeholder */}
         <header className="h-14 bg-white border-b border-slate-200 flex items-center px-4">
           Header
         </header>
 
-        {/* Page content */}
         <section className="flex-1 p-4">{children}</section>
       </main>
     </div>
